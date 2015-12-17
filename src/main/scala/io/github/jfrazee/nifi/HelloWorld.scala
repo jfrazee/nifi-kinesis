22b77a1f6fbc0d02466a1b0bd9ced81b2e4f247e
Joey Frazee <joey.frazee@icloud.com>
2015-12-17 01:41:09 -0600

    Initial commit

---
+++ src/main/scala/io/github/jfrazee/nifi/HelloWorld.scala
@@ -0,0 +1,7 @@
+package io.github.jfrazee.nifi
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!!!")
+  }
+}
---
+++ src/test/scala/io/github/jfrazee/nifi/HelloWorldSpec.scala
@@ -0,0 +1,11 @@
+package io.github.jfrazee.nifi
+
+import org.scalatest.FunSpec
+
+class HelloWorldSpec extends FunSpec {
+  describe("Adding 1 to 1") {
+    it("should equals 2") {
+      assert(1 + 1 == 2)
+    }
+  }
+}
